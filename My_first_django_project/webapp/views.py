d25ab7d8471b18d2f9665820288f798c4d8a86c0
Tejasw s <38702467+tejas7125@users.noreply.github.com>
2019-03-08 11:34:48 +0530

    Add files via upload

---
+++ My_first_django_project/webapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse('<H2> HEY! Welcome to My Webpage </H2>')
